c10a475409076bd61241e9a5f8398a4e0622a02f
msjung75 <msjung75@gmail.com>
2026-09-24 11:48:37 +0900

    Configure registered Microsoft application for OneDrive sync

--- onedrive-config.js
+++ onedrive-config.js
@@ -1,2 +1,2 @@
 // Public application ID only. Never put a client secret or access token here.
-window.UNMYEONG_ONEDRIVE_CLIENT_ID = '';
+window.UNMYEONG_ONEDRIVE_CLIENT_ID = 'c149294e-e4d7-4f8e-a6ce-0c79401c9c11';
